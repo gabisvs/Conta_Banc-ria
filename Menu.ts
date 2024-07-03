8a5c7d64030d7f46322c58b775747a4ed850b431
Gabrielli <gabivirgilio@oultook.com>
2024-07-03 11:34:38 -0300

    Classe Conta

--- Menu.ts
+++ Menu.ts
@@ -1,9 +1,45 @@
 import readlinesync = require("readline-sync");
 import { colors } from './src/util/Colors';
+import { Conta } from "./src/model/Conta";
 
 export function main() {
 
     let opcao: number;
+
+    // Novas Instâncias da Classe Conta (Objetos)
+    const c1: Conta = new Conta(1, 1234, 1, 'Gabrielli Virgilio', 800000.00);
+    const c2: Conta = new Conta(2, 1234, 2, 'Júlia Castro', 600000.00);
+
+    // Visualizando os dados da Conta c1
+    c1.visualizar();
+    
+    // Visualizando os dados da Conta c2
+    c2.visualizar();
+
+    // Visualizando o saldo da Conta c1
+    console.log(`\nO Saldo da conta 01 é: ${c1.saldo}`)
+
+   // Alterando o Saldo da Conta c2
+    c2.saldo = 900000.00;
+
+    // Visualizando o saldo da Conta c2 após alteração
+    console.log(`\nO Saldo da conta 02 é: ${c2.saldo}`)
+
+    // Saque nas Contas 
+    console.log(`Sacar 100.00 reais da Conta c1: ${c1.sacar(100)}`); //true
+    c1.visualizar;
+
+    console.log(`Sacar 1000000.00 reais da Conta c2: ${c2.sacar(1000000)}`); //false
+    c2.visualizar();
+
+    // Depósito nas Contas 
+    console.log(`Depositar 200000.00 reais da Conta c1: `);
+    c1.depositar(200000.00)
+    c1.visualizar();
+
+    console.log(`Depositar 300000.00 reais da Conta c2: `); 
+    c1.depositar(300000.25)
+    c2.visualizar();
 
     while (true) {
 
@@ -105,7 +141,7 @@ function sobre(): void {
     console.log("Projeto Desenvolvido por: Gabrielli ");
     console.log("Generation Brasil - gabivirgilio@oultook.com ");
     console.log("https://github.com/gabisvs");
-    console.log("¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨");
+    console.log("¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨");
 }
 
 function keyPress(): void {
